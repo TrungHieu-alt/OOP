bc1a299b3862cc6d70ce49a3e66c5f6492898b81
8uandj <frankhoang231204@gmail.com>
2025-08-18 13:53:40 +0700

    fix: fix LoginController

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,6 +1,7 @@
 package controller;
 
 
+import exceptions.AuthenticationException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -8,12 +9,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import repository.UserRepository;
+import services.UserService;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+/**
+ * Controller class for managing the login view.
+ */
 public class LoginController implements Initializable {
     @FXML
     private Button btn_login;
@@ -33,11 +38,21 @@ public class LoginController implements Initializable {
     @FXML
     private Button btn_login_admin;
 
+    private UserService userService;
+    private static final UserRepository userRepository = new UserRepository();
 
+    /**
+     * Initializes the controller class.
+     *
+     * @param url the location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle the resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         txt_username.setFocusTraversable(false);
         txt_password.setFocusTraversable(false);
+
+        this.userService = new UserService(userRepository);
 
         btn_login.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -47,10 +62,8 @@ public class LoginController implements Initializable {
                 } else if (!txt_username.getText().trim().isEmpty() && (!txt_password.getText().trim().isEmpty())){
                     wrongLogin.setText("Wrong username or password");
                     try {
-                        DB.logInUser(event, txt_username.getText(), txt_password.getText());
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
-                    } catch (IOException e) {
+                        userService.logInUser(event, txt_username.getText(), txt_password.getText());
+                    } catch (AuthenticationException e) {
                         throw new RuntimeException(e);
                     }
                 }
@@ -62,17 +75,7 @@ public class LoginController implements Initializable {
             @Override
             public void handle(ActionEvent event) {
                 try {
-                    DB.changeScene(event, "/view/SignUp.fxml", "Quản lý thư viện", null, null, null);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        btn_login_admin.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                try {
-                    DB.changeScene(event, "/view/LoginAdmin.fxml", "Quản lý thư viện", null, null, null);
+                    userService.changeToSignUp(event);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
